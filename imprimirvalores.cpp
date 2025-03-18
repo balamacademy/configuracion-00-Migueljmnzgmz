b44ac3753e6f909f630d8c8190dc5fc8017b089b
Migueljmnzgmz <mj601586@gmail.com>
2025-03-18 07:45:14 -0600

    Add files via upload

---
+++ imprimirvalores.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main (){;
+    string nombre = "Juan";
+    int edad = 34;
+
+    cout << "El nombre es " << nombre << " de edad " << edad;
+
+    return 0;
+}
---
+++ ingresarvalores.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main(){;
+    string nombre;
+    int edad;
+    cout << "Dime primero tu nombre y despues tu edad" << "\n";
+    cin >> nombre >> edad;
+    cout << "El nombre es " << nombre << " de edad " << edad;
+
+
+
+
+    return 0;
+}
